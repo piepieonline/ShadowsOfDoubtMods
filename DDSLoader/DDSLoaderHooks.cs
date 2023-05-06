0ab50eabb5123369a5df5364b2dc0a5a9e7901dd
Piepieonline <piepieonline@gmail.com>
2023-05-07 08:52:21 +1000

    Added a generic DDSLoader mod, for DDS and strings Removed Unhollower from where it's not needed

---
+++ DDSLoader/DDSLoaderHooks.cs
@@ -0,0 +1,74 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace DDSLoader
+{
+    internal class DDSLoaderHooks
+    {
+        [HarmonyPatch(typeof(Toolbox), "Start")]
+        public class Toolbox_Start
+        {
+            public static void Postfix()
+            {
+                foreach(var dir in DDSLoader.DDSLoaderPlugin.modsToLoadFrom)
+                {
+                    var treesPath = Path.Combine(dir.FullName, "DDS", "Trees");
+                    var messagesPath = Path.Combine(dir.FullName, "DDS", "Messages");
+                    var blocksPath = Path.Combine(dir.FullName, "DDS", "Blocks");
+
+                    if(Directory.Exists(treesPath) )
+                    {
+                        foreach (var treePath in Directory.GetFiles(treesPath, "*.tree"))
+                        {
+                            var message = JsonUtility.FromJson<DDSSaveClasses.DDSTreeSave>(File.ReadAllText(treePath));
+                            Toolbox.Instance.allDDSTrees.Add(message.id, message);
+                        }
+                    }
+
+                    if (Directory.Exists(messagesPath))
+                    {
+                        foreach (var messagePath in Directory.GetFiles(messagesPath, "*.msg"))
+                        {
+                            var message = JsonUtility.FromJson<DDSSaveClasses.DDSMessageSave>(File.ReadAllText(messagePath));
+                            Toolbox.Instance.allDDSMessages.Add(message.id, message);
+                        }
+                    }
+
+                    if (Directory.Exists(blocksPath))
+                    {
+                        foreach (var blockPath in Directory.GetFiles(blocksPath, "*.block"))
+                        {
+                            var block = JsonUtility.FromJson<DDSSaveClasses.DDSBlockSave>(File.ReadAllText(blockPath));
+                            Toolbox.Instance.allDDSBlocks.Add(block.id, block);
+                        }
+                    }
+
+                    DDSLoaderPlugin.Logger.LogInfo($"Loaded DDS Content For: {dir.Parent.Name}");
+
+                    var selectedLanguagePath = Path.Combine(dir.FullName, "Strings", Game.Instance.language);
+                    var englishLanguagePath = Path.Combine(dir.FullName, "Strings", "English");
+
+                    var stringsPath = Directory.Exists(selectedLanguagePath) ? selectedLanguagePath : Directory.Exists(englishLanguagePath) ? englishLanguagePath : "";
+                    if(stringsPath != "")
+                    {
+                        foreach(var stringFile in Directory.GetFiles(stringsPath, "*.csv", SearchOption.AllDirectories))
+                        {
+                            var fileName = Path.GetFileNameWithoutExtension(stringFile);
+                            foreach (var line in File.ReadAllLines(stringFile))
+                            {
+                                var lineSplit = line.Split(",");
+                                Strings.WriteToDictionary(fileName, lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], int.TryParse(lineSplit[4], out var frequency) ? frequency : 0, bool.TryParse(lineSplit[5], out var requiresSuffix) ? requiresSuffix : false, lineSplit[6]);
+                            }
+                        }
+
+                        DDSLoaderPlugin.Logger.LogInfo($"Loaded String Content For: {dir.Parent.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DDSLoader/DDSLoaderPlugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DDSLoader
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class DDSLoaderPlugin : BasePlugin
+    {
+        public static ManualLogSource Logger;
+
+        public static List<DirectoryInfo> modsToLoadFrom = new List<DirectoryInfo>();
+
+        public override void Load()
+        {
+            Logger = Log;
+
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            var harmony = new Harmony($"{PluginInfo.PLUGIN_GUID}");
+            harmony.PatchAll();
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is patched!");
+
+            modsToLoadFrom = Directory.GetDirectories(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), ".."), "DDSContent", SearchOption.AllDirectories).Select(dir => new DirectoryInfo(dir)).ToList(); ;
+        }
+    }
+}
