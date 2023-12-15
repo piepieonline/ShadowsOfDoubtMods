91d81b6635f8a5b6d51c627055e027298ca902ae
Piepieonline <piepieonline@gmail.com>
2023-12-16 10:15:43 +1100

    Added an example mod that works on both Mono and Il2CPP branches

---
+++ ExampleBranchSafeMod/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using UnityEngine;
+
+#if MONO
+using BepInEx.Unity.Mono;
+#elif IL2CPP
+using BepInEx.Unity.IL2CPP;
+#endif
+
+namespace ExampleBranchSafeMod
+{
+#if MONO
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class ExampleBranchSafeModPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"I'm in Mono2!");
+        }
+    }
+#elif IL2CPP
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class ExampleBranchSafeModPlugin : BasePlugin
+{
+    public override void Load()
+    {
+        Log.LogInfo($"I'm in IL2CPP2!");
+    }
+}
+#endif
+}
